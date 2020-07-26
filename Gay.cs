42a4b0406be702cd4087cdac6b2fb73887756991
Yuuki MJ <53755879+yohamaruki@users.noreply.github.com>
2020-07-26 11:48:41 +0800

    Add files via upload
    Why.

---
+++ Gay.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace output
+{
+    class Gay
+    {
+        static void Main(string[] args)
+        {
+            while(true) {
+                Console.WriteLine("Press 1 to know something good");
+                int bruhMoment = Int32.Parse(Console.ReadLine());
+
+                switch (bruhMoment) { 
+                    case 1: 
+                    if (bruhMoment == 1) {
+                        Console.WriteLine("Haha you're fuckin' gay");
+                        System.Environment.Exit(1);
+                    }
+                    break;
+                    case 2: 
+                    if (bruhMoment < 1) {
+                        Console.WriteLine("Omg what I said 1");
+                        return;
+                    }
+                    break;
+                    case 3: 
+                    if (bruhMoment > 1) {
+                        Console.WriteLine("I SAID ONE. 1.");
+                        return;
+                    }
+                    break;
+
+                    default: 
+                    Console.WriteLine("Pick 1 :eyes:");
+                    continue;
+                }
+            }
+        }
+    }
+}
